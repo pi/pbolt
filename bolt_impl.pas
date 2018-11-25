c24eda5bfb70c89278005780c834dbf90d66ea6b
Vif Ardente <vif.ardente@gmail.com>
2018-11-25 22:12:27 +0300

    retabbing

--- bolt_impl.pas
+++ bolt_impl.pas
@@ -9,42 +9,42 @@ uses bolt;
 // Options represents the options that can be set when opening a database.
 type
   TOptions = record
-	  // Timeout is the amount of time to wait to obtain a file lock.
-	  // When set to zero it will wait indefinitely. This option is only
-	  // available on Darwin and Linux.
-	  Timeout: cardinal;
+    // Timeout is the amount of time to wait to obtain a file lock.
+    // When set to zero it will wait indefinitely. This option is only
+    // available on Darwin and Linux.
+    Timeout: cardinal;
 
-	  // Sets the DB.NoGrowSync flag before memory mapping the file.
-	  NoGrowSync: boolean;
+    // Sets the DB.NoGrowSync flag before memory mapping the file.
+    NoGrowSync: boolean;
 
-	  // Do not sync freelist to disk. This improves the database write performance
-	  // under normal operation, but requires a full database re-sync during recovery.
-	  NoFreelistSync: boolean;
+    // Do not sync freelist to disk. This improves the database write performance
+    // under normal operation, but requires a full database re-sync during recovery.
+    NoFreelistSync: boolean;
 
-	  // Open database in read-only mode. Uses flock(..., LOCK_SH |LOCK_NB) to
-	  // grab a shared lock (UNIX).
-	  ReadOnly: boolean;
+    // Open database in read-only mode. Uses flock(..., LOCK_SH |LOCK_NB) to
+    // grab a shared lock (UNIX).
+    ReadOnly: boolean;
 
-	  // Sets the DB.MmapFlags flag before memory mapping the file.
-	  MmapFlags: integer;
+    // Sets the DB.MmapFlags flag before memory mapping the file.
+    MmapFlags: integer;
 
-	  // InitialMmapSize is the initial mmap size of the database
-	  // in bytes. Read transactions won't block write transaction
-	  // if the InitialMmapSize is large enough to hold database mmap
-	  // size. (See DB.Begin for more information)
-	  //
-	  // If <=0, the initial map size is 0.
-	  // If initialMmapSize is smaller than the previous database size,
-	  // it takes no effect.
-	  InitialMmapSize: SizeUint;
+    // InitialMmapSize is the initial mmap size of the database
+    // in bytes. Read transactions won't block write transaction
+    // if the InitialMmapSize is large enough to hold database mmap
+    // size. (See DB.Begin for more information)
+    //
+    // If <=0, the initial map size is 0.
+    // If initialMmapSize is smaller than the previous database size,
+    // it takes no effect.
+    InitialMmapSize: SizeUint;
 
-	  // PageSize overrides the default OS page size.
-	  PageSize: SizeUint;
+    // PageSize overrides the default OS page size.
+    PageSize: SizeUint;
 
-	  // NoSync sets the initial value of DB.NoSync. Normally this can just be
-	  // set directly on the DB itself when returned from Open(), but this option
-	  // is useful in APIs which expose Options but not the underlying DB.
-	  NoSync: boolean;
+    // NoSync sets the initial value of DB.NoSync. Normally this can just be
+    // set directly on the DB itself when returned from Open(), but this option
+    // is useful in APIs which expose Options but not the underlying DB.
+    NoSync: boolean;
   end;
   POptions = ^TOptions;
 
@@ -72,9 +72,9 @@ begin
 end;
 
 const
-	DefaultMaxBatchSize   = 1000;
-	DefaultMaxBatchDelay  = 10;
-	DefaultAllocSize      = 16 * 1024 * 1024;
+  DefaultMaxBatchSize   = 1000;
+  DefaultMaxBatchDelay  = 10;
+  DefaultAllocSize      = 16 * 1024 * 1024;
 
 var
   DefaultOptions: TOptions; // all zeros
@@ -89,24 +89,24 @@ type
   pgid = uint64;
 
   inode = record
-	  flags : cardinal;
-	  pgid  : pgid;
-	  key   : array of byte;
-	  value : array of byte;
+    flags : cardinal;
+    pgid  : pgid;
+    key   : array of byte;
+    value : array of byte;
   end;
   pinode = ^inode;
 
   node = class
   public
-	  bucket     : TBucket;
-	  isLeaf     : boolean;
-	  unbalanced : boolean;
-	  spilled    : boolean;
-	  key        : pbyte;
-	  pgid       : pgid;
-	  parent     : node;
-	  children   : array of node;
-	  inodes     : array of pinode;
+    bucket     : TBucket;
+    isLeaf     : boolean;
+    unbalanced : boolean;
+    spilled    : boolean;
+    key        : pbyte;
+    pgid       : pgid;
+    parent     : node;
+    children   : array of node;
+    inodes     : array of pinode;
   end;
 
   page = record
@@ -141,31 +141,31 @@ type
   end;
 
   meta = record
-	  magic    : cardinal;
-	  version  : cardinal;
-	  pageSize : cardinal;
-	  flags    : cardinal;
-	  root     : bucket;
-	  freelist : pgid;
-	  pgid     : pgid;
-	  txid     : txid;
-	  checksum : uint64;
+    magic    : cardinal;
+    version  : cardinal;
+    pageSize : cardinal;
+    flags    : cardinal;
+    root     : bucket;
+    freelist : pgid;
+    pgid     : pgid;
+    txid     : txid;
+    checksum : uint64;
   end;
   Pmeta = ^meta;
 
   TtxPending = class
   public
-	  ids              : array of pgid;
-	  alloctx          : array of txid; // txids allocating the ids
-	  lastReleaseBegin : txid;   // beginning txid of last matching releaseRange
+    ids              : array of pgid;
+    alloctx          : array of txid; // txids allocating the ids
+    lastReleaseBegin : txid;   // beginning txid of last matching releaseRange
   end;
 
   TFreelist = class
   private
-	  ids     : array of pgid;  // all free and available free page ids.
-	  allocs  : TQQHash;      // mapping of txid that allocated a pgid.
-	  pending : TQHash;         // mapping of soon-to-be free page ids by tx.
-	  cache   : TQHashSet;          // fast lookup of all free and pending page ids.
+    ids     : array of pgid;  // all free and available free page ids.
+    allocs  : TQQHash;      // mapping of txid that allocated a pgid.
+    pending : TQHash;         // mapping of soon-to-be free page ids by tx.
+    cache   : TQHashSet;          // fast lookup of all free and pending page ids.
   public
     constructor Create;
     destructor Destroy; override;
@@ -178,18 +178,18 @@ type
     root      : pgid;
     sequence  : uint64;
     // Bucket part
-	  tx        : TTx;    // the associated transaction
-	  buckets   : TSHash; // subbucket cache
-	  page      : Ppage;  // inline page reference
-	  rootNode  : node;   // materialized node for the root page.
-	  nodes     : TQHash; // node cache
+    tx        : TTx;    // the associated transaction
+    buckets   : TSHash; // subbucket cache
+    page      : Ppage;  // inline page reference
+    rootNode  : node;   // materialized node for the root page.
+    nodes     : TQHash; // node cache
 
-	  // Sets the threshold for filling nodes when they split. By default,
-	  // the bucket will fill to 50% but it can be useful to increase this
-	  // amount if you know that your write workloads are mostly append-only.
-	  //
-	  // This is non-persisted across transactions so it must be set in every Tx.
-	  FillPercent : double;
+    // Sets the threshold for filling nodes when they split. By default,
+    // the bucket will fill to 50% but it can be useful to increase this
+    // amount if you know that your write workloads are mostly append-only.
+    //
+    // This is non-persisted across transactions so it must be set in every Tx.
+    FillPercent : double;
 
   protected
     procedure Dereference;
@@ -210,11 +210,11 @@ type
     fWritable      : boolean;
     managed        : boolean;
     fDb            : TDB;
-	  meta           : Pmeta;
-	  root           : TBucket;
-	  pages          : TQPtrHash; //map[pgid]*page
-	  stats          : TtxStats;
-	  commitHandlers : array of TCommitHandler;
+    meta           : Pmeta;
+    root           : TBucket;
+    pages          : TQPtrHash; //map[pgid]*page
+    stats          : TtxStats;
+    commitHandlers : array of TCommitHandler;
   public
     WriteFlag : integer;
   public // interface methods
@@ -240,13 +240,13 @@ type
     fOpened: boolean;
     fFd: integer;
     fWritable: boolean;
-  	fNoSync: boolean;
-  	fNoGrowSync: boolean;
-  	fMmapFlags: integer;
-  	fNoFreelistSync: boolean;
-  	fMaxBatchSize: SizeUint;
-  	fMaxBatchDelay: integer;
-  	fAllocSize: SizeUint;
+    fNoSync: boolean;
+    fNoGrowSync: boolean;
+    fMmapFlags: integer;
+    fNoFreelistSync: boolean;
+    fMaxBatchSize: SizeUint;
+    fMaxBatchDelay: integer;
+    fAllocSize: SizeUint;
     fPageSize: SizeUint;
     fMmapLock: TRtlCriticalSection;
 
@@ -438,36 +438,36 @@ var
   m: Pmeta;
 begin
   SetLength(buf, fPageSize * 4);
-	// Create two meta pages on a buffer.
-	for i := 0 to 1 do
+  // Create two meta pages on a buffer.
+  for i := 0 to 1 do
   begin
     p := Ppage(@buf[fPageSize * i]);
-		p^.id := pgid(i);
-		p^.flags := metaPageFlag;
+    p^.id := pgid(i);
+    p^.flags := metaPageFlag;
 
-		// Initialize the meta page.
-		m := Pmeta(@p^.ptr);
-		m^.magic := magic;
-		m^.version := version;
-		m^.pageSize := uint32(fPageSize);
-		m^.freelist := 2;
-		m^.root.root := 3;
-		m^.pgid := 4;
-		m^.txid := txid(i);
-		m^.checksum := meta_checksum(m);
+    // Initialize the meta page.
+    m := Pmeta(@p^.ptr);
+    m^.magic := magic;
+    m^.version := version;
+    m^.pageSize := uint32(fPageSize);
+    m^.freelist := 2;
+    m^.root.root := 3;
+    m^.pgid := 4;
+    m^.txid := txid(i);
+    m^.checksum := meta_checksum(m);
   end;
 
-	// Write an empty freelist at page 3.
-	p := Ppage(@buf[fPageSize * 2]);
-	p^.id := pgid(2);
-	p^.flags := freelistPageFlag;
-	p^.count := 0;
+  // Write an empty freelist at page 3.
+  p := Ppage(@buf[fPageSize * 2]);
+  p^.id := pgid(2);
+  p^.flags := freelistPageFlag;
+  p^.count := 0;
 
-	// Write an empty leaf page at page 4.
-	p := Ppage(@buf[fPageSize * 3]);
-	p^.id := pgid(3);
-	p^.flags := leafPageFlag;
-	p^.count := 0;
+  // Write an empty leaf page at page 4.
+  p := Ppage(@buf[fPageSize * 3]);
+  p^.id := pgid(3);
+  p^.flags := leafPageFlag;
+  p^.count := 0;
 
   FileSeek(fFd, 0, fsFromBeginning);
   FileWrite(fFd, buf[0], fPageSize * 4);
@@ -479,34 +479,34 @@ var
   i: cardinal;
   remainder: int64;
 begin
-	// Double the size from 32KB until 1GB.
-	for i := 15 to 30 do
+  // Double the size from 32KB until 1GB.
+  for i := 15 to 30 do
   begin
-		if size <= (1 shl i) then
+    if size <= (1 shl i) then
     begin
-			result := 1 shl i;
+      result := 1 shl i;
       exit;
     end;
   end;
 
-	// Verify the requested size is not above the maximum allowed.
-	if size > maxMapSize then
-		Error('mmap too large');
+  // Verify the requested size is not above the maximum allowed.
+  if size > maxMapSize then
+    Error('mmap too large');
 
-	// If larger than 1GB then grow by 1GB at a time.
+  // If larger than 1GB then grow by 1GB at a time.
   remainder := size mod int64(maxMmapStep);
-	if remainder > 0 then
-		inc(size, int64(maxMmapStep) - remainder);
+  if remainder > 0 then
+    inc(size, int64(maxMmapStep) - remainder);
 
-	// Ensure that the mmap size is a multiple of the page size.
-	// This should always be true since we're incrementing in MBs.
-	pageSize := int64(db.pageSize)
-	if (size mod fPageSize) <> 0 then
-		size := ((size div int64(fPageSize)) + 1) * int64(fPageSize);
+  // Ensure that the mmap size is a multiple of the page size.
+  // This should always be true since we're incrementing in MBs.
+  pageSize := int64(db.pageSize)
+  if (size mod fPageSize) <> 0 then
+    size := ((size div int64(fPageSize)) + 1) * int64(fPageSize);
 
-	// If we've exceeded the max size then only grow up to the max size.
-	if size > maxMapSize then
-		size := maxMapSize;
+  // If we've exceeded the max size then only grow up to the max size.
+  if size > maxMapSize then
+    size := maxMapSize;
 
   result := size;
 end;
@@ -552,15 +552,15 @@ begin
   if opt = nil then
     opt := @DefaultOptions;
 
-	fNoSync := opt^.NoSync;
-	fNoGrowSync := opt^.NoGrowSync;
-	fMmapFlags := opt^.MmapFlags;
-	fNoFreelistSync := opt^.NoFreelistSync;
+  fNoSync := opt^.NoSync;
+  fNoGrowSync := opt^.NoGrowSync;
+  fMmapFlags := opt^.MmapFlags;
+  fNoFreelistSync := opt^.NoFreelistSync;
 
-	// Set default values for later DB operations.
-	fMaxBatchSize := DefaultMaxBatchSize;
-	fMaxBatchDelay := DefaultMaxBatchDelay;
-	fAllocSize := DefaultAllocSize;
+  // Set default values for later DB operations.
+  fMaxBatchSize := DefaultMaxBatchSize;
+  fMaxBatchDelay := DefaultMaxBatchDelay;
+  fAllocSize := DefaultAllocSize;
 
   if opt^.ReadOnly then
     mode := fmOpenRead or fmShareExclusive
@@ -572,8 +572,8 @@ begin
     mode := fmOpenReadWrite;
   end;
 
-	// Open data file and separate sync handler for metadata writes.
-	fPath := aFileName;
+  // Open data file and separate sync handler for metadata writes.
+  fPath := aFileName;
 
   fFd := FileOpen(aFileName, mode or fmShareExclusive);
   if fFd = THandle(-1) then
@@ -593,43 +593,43 @@ begin
   end;
 
 (*
-	// Initialize page pool.
-	db.pagePool = sync.Pool{
-		New: func() interface{} {
-			return make([]byte, db.pageSize)
-		},
-	}
+  // Initialize page pool.
+  db.pagePool = sync.Pool{
+    New: func() interface{} {
+      return make([]byte, db.pageSize)
+    },
+  }
 *)
 
 (*
-	// Memory map the data file.
-	if err := db.mmap(options.InitialMmapSize); err != nil {
-		_ = db.close()
-		return nil, err
-	}
+  // Memory map the data file.
+  if err := db.mmap(options.InitialMmapSize); err != nil {
+    _ = db.close()
+    return nil, err
+  }
 
-	if db.readOnly {
-		return db, nil
-	}
+  if db.readOnly {
+    return db, nil
+  }
 *)
 
-	db.loadFreelist()
+  db.loadFreelist()
 
-	// Flush freelist when transitioning from no sync to sync so
-	// NoFreelistSync unaware boltdb can open the db later.
-	if !db.NoFreelistSync && !db.hasSyncedFreelist() {
-		tx, err := db.Begin(true)
-		if tx != nil {
-			err = tx.Commit()
-		}
-		if err != nil {
-			_ = db.close()
-			return nil, err
-		}
-	}
+  // Flush freelist when transitioning from no sync to sync so
+  // NoFreelistSync unaware boltdb can open the db later.
+  if !db.NoFreelistSync && !db.hasSyncedFreelist() {
+    tx, err := db.Begin(true)
+    if tx != nil {
+      err = tx.Commit()
+    }
+    if err != nil {
+      _ = db.close()
+      return nil, err
+    }
+  }
 
-	// Mark the database as opened and return.
-	return db, nil
+  // Mark the database as opened and return.
+  return db, nil
 }
 
 end;
